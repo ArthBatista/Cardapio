42e7c7dd81d89cdcaf8d48db2fdff3d2f4b4af97
Arthur Batista <78709663+ArthBatista@users.noreply.github.com>
2021-07-24 17:10:33 -0300

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,9 +13,9 @@ const Footer: React.FC = () => {
          ° Ervilha,Presunto, Salsicha <br />
          ° Tomate, cebola</p>
       <h2>2° Molhos</h2>
-      <p>° Molho branco <br />
-         ° Molho Vermelho<br />
-         ° Molho a bolonhesa</p>
+      <p>° Molho Vermelho<br />
+         ° Molho bolonhesa<br />
+         ° Molho branco</p>
          <h2>3° Macarrão</h2>
          <p>° Espaguete     <br />
             ° Talharim      <br />
@@ -24,11 +24,10 @@ const Footer: React.FC = () => {
             </p>
             <div className="oe"> <p>Pronto! Seu macarrão sera entregue em breve.</p> </div>
             <div className="oa"> <h3>Bebidas</h3></div>
-            <p>Sucos: <br />
               ° Laranja_______° Coca 1L <br />
               ° Maracujá______° Guaraná 1L <br />
-              ° Acerola_______° Coca 350ml<br />
-              ° Goiaba________° Guaraná 450ml</p>                
+              ° Acerola_______° Coca 350ml(LATA)<br />
+              ° Goiaba________° Guaraná 350ml(LATA)</p>                
       </div> 
     </Container>
   );
